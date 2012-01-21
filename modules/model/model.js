116b994ca3d8e9072be62260c6a9e2e99f862f71
Scott Burch <scott@toto.bulldoginfo.com>
2012-01-21 14:49:49 -0800

    eliminated Application.fireEvent

--- modules/model/model.js
+++ modules/model/model.js
@@ -4,29 +4,29 @@ defineModule({name:'model', category:'river', description:'Model constructors', 
     that.Property = Property;
 
     that.childAdded = function(child) {
-        Application.fireEvent('model', 'childAdded', {child:child});
+        that.fireEvent('childAdded', {child:child});
     };
 
 
     that.childRemoved = function(child, parent) {
-        Application.fireEvent('model', 'childRemoved', {child: child, parent:parent});
+        that.fireEvent('childRemoved', {child: child, parent:parent});
     };
 
     that.propertyAdded = function(prop) {
-        Application.fireEvent('model', 'propertyAdded', {prop:prop});
+        that.fireEvent('propertyAdded', {prop:prop});
     };
 
 
     that.propertyRemoved = function(data) {
-        Application.fireEvent('model', 'propertyRemoved', data);
+        that.fireEvent('propertyRemoved', data);
     };
 
     that.propertyValueUpdated = function (prop) {
-        Application.fireEvent('model', 'propertyValueUpdated', prop);
+        that.fireEvent('propertyValueUpdated', prop);
     };
 
     that.propertyNewValueUpdated = function (prop) {
-        Application.fireEvent('model', 'propertyNewValueUpdated', prop);
+        that.fireEvent('propertyNewValueUpdated', prop);
     };
 
 
