97d71396b15d278bf90f7f4501fe672d898a3bca
Scott Burch <scott@toto.bulldoginfo.com>
2012-01-21 15:06:54 -0800

    eliminated Application.doAction

--- modules/eventLogging/eventLogging.js
+++ modules/eventLogging/eventLogging.js
@@ -1,7 +1,7 @@
 defineModule({name:'eventLogging', category:'logging', description:'Event logging'}, function(that) {
 
     that.on_moduleManager_eventFired = function(data) {
-        Application.doAction('log', ['Event: '+data.moduleName, data.event, data.data]);
+        that.doAction('log', ['Event: '+data.moduleName, data.event, data.data]);
     }
 
     return that;
