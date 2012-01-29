df801df33fd19808e0c405814c1fbc0b3b99c0ca
Scott Burch <scott@toto.bulldoginfo.com>
2012-01-29 10:19:39 -0800

    moving modules to other repositories

--- modules/logging/logging.js
+++
@@ -1,8 +0,0 @@
-defineModule({name:'logging', category:'logging', description:'Logging module'}, function(that) {
-    that.do_log = function(data) {
-        data = Array.isArray(data) ? data : [data];
-        if(window.console && console.log) {
-            console.log(data);
-        }
-    };
-});
