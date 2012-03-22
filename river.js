43cd6418efde77b970dad2827e0147a36793a23d
Scott Burch <scott@toto.bulldoginfo.com>
2012-03-22 06:20:11 -0700

    added new river bootstrap file

---
+++ river.js
@@ -0,0 +1,18 @@
+(function() {
+    "use strict";
+    window.river = function(config, cb) {
+        setupRequireConfig();
+
+        require(['river/lib/Application'], function(App) {
+            App.start(config.modulesPath, config.modules, cb);
+        });
+
+        function setupRequireConfig() {
+            window.requireConfig = window.requireConfig || {paths:{}};
+            if(config.riverPath) {
+                requireConfig.paths.river = config.riverPath;
+            }
+            require.config(requireConfig);
+        }
+    }
+}());
