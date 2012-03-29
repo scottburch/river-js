105a048ae30dcc5f3e56de7f9e15ed67bbd19b7f
Scott Burch <scott@bulldoginfo.com>
2012-03-28 22:52:00 -0700

    plugin list works

--- tools/groovy/src/river.groovy
+++ tools/groovy/src/river.groovy
@@ -1,10 +1,21 @@
+import groovyx.net.http.*
+import static groovyx.net.http.ContentType.*
+import static groovyx.net.http.Method.*
+
 def config = new Config()
 def cmd = this.args[0]
 
 
 this['list-plugins'] = {
-
-    println 'list plugins'
+    def http = new HTTPBuilder(config.config.moduleListHost);
+    http.request( GET, JSON) {
+        uri.path = "/${config.config.moduleListPath}"
+        response.success = {resp, json ->
+            json.plugins.each {
+                println "${it.name} ${it.ver} - ${it.author}"
+            }
+        }
+    }
 }
 
 if(cmd) {
