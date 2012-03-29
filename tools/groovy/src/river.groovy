db99364188d62dc3bb96eb2517adc376b25aa440
Scott Burch <scott@bulldoginfo.com>
2012-03-28 23:09:45 -0700

    added installation instructions

--- tools/groovy/src/river.groovy
+++ tools/groovy/src/river.groovy
@@ -2,25 +2,34 @@ import groovyx.net.http.*
 import static groovyx.net.http.ContentType.*
 import static groovyx.net.http.Method.*
 
-def config = new Config()
+this.config = new Config()
 def cmd = this.args[0]
 
 
 this['list-plugins'] = {
-    def http = new HTTPBuilder(config.config.moduleListHost);
-    http.request( GET, JSON) {
-        uri.path = "/${config.config.moduleListPath}"
-        response.success = {resp, json ->
-            json.plugins.each {
-                println "${it.name} ${it.ver} - ${it.author}"
-            }
-        }
+    def json = getPluginConfig();
+    json.plugins.each {
+        println "${it.name} ${it.ver} - ${it.author}"
     }
+}
+
+this['install-plugin'] = {
+
 }
 
 if(cmd) {
     this[cmd]()
 } else {
     println "Please enter a command"
+}
+
+private getPluginConfig() {
+    def http = new HTTPBuilder(config.config.moduleListHost);
+    http.request( GET, JSON) {
+        uri.path = "/${config.config.moduleListPath}"
+        response.success = {resp, json ->
+            return json
+        }
+    }
 }
 
